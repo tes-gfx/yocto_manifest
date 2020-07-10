d52f9b826f2ecefe319f6482319fc42771d7da81
Christian Thaler <christian.thaler@tes-dst.com>
2020-07-10 15:09:40 +0200

    add script to setup build environment

---
+++ setup_build.sh
@@ -0,0 +1,23 @@
+TARGET=$2
+BUILD_DIR=./build_$TARGET
+
+if [ $# -ne 1 ]
+then
+	echo Single target parameter needed. Choose one of:
+	echo   a10    - Arria 10
+	echo   s10    - Stratix 10
+	exit 1
+fi
+
+. ./poky/oe-init-build-env $BUILD_DIR
+
+cp ../templates/svn.inc ./conf/
+cp ../templates/bblayers.conf ./conf/
+cp ../templates/local_$TARGET.conf ./conf/local.conf
+
+cd ..
+echo YOCTOROOT = \"$(pwd)\" >> $BUILD_DIR/conf/bblayers.conf
+
+echo -------------------------------------------------------------------------------
+echo Source Bitbake environment with . ./poky/oe-init-build-env build_$TARGET $BUILD_DIR
+echo Please update $BUILD_DIR/conf/svn.inc
--- setup_repos.sh
+++ setup_repos.sh
@@ -2,16 +2,16 @@ RELEASE=dunfell
 
 git clone -b $RELEASE git://git.yoctoproject.org/poky
 
+mkdir -p downloads
 mkdir -p repos
 pushd repos
 git clone -b $RELEASE git@github.com:openembedded/meta-openembedded.git
-git clone -b $RELEASE git@github.com:c-thaler/meta-tes.git
-git clone -b $RELEASE git@github.com:c-thaler/meta-qt5.git
-git clone -b $RELEASE git@github.com:c-thaler/meta-altera.git
+git clone -b $RELEASE git@github.com:tes-gfx/meta-tes.git
+git clone -b master git@github.com:tes-gfx/meta-altera.git
 pushd meta-altera
 git remote add upstream git@github.com:kraj/meta-altera
 popd
-git clone -b $RELEASE git@github.com:c-thaler/meta-arm
+git clone -b $RELEASE git@github.com:tes-gfx/meta-arm
 pushd meta-arm
 git remote add upstream git://git.yoctoproject.org/meta-arm
 popd
